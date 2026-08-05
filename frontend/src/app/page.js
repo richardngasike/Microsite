f1c5d8f5fae08ce7f083493f471460ba778ec123
richardngasike <richardmax254@gmail.com>
2026-08-05 10:47:12 +0300

    Fixing GC8 image jpg

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -69,7 +69,7 @@ export default function HomePage() {
           </Link>
 
           <Link href="/gc8" className={styles.tile}>
-            <img src="/images/3D9A5657.jpg" alt="" className={styles.tileImg} />
+            <img src="/images/GC8.jpg" alt="" className={styles.tileImg} />
             <div className={styles.tileScrim} />
             <div className={styles.tileBody}>
               <span className={styles.tileEyebrow}>Global Fund</span>
